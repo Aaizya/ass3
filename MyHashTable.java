23faa6502f3e40a6961b98c907d25aa49839d566
Aruzhan <aruzhan1471@gmail.com>
2023-05-18 05:53:48 +0600

    2

--- MyHashTable.java
+++ MyHashTable.java
@@ -1,97 +1,144 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyHashTable<K, V> {
-
-    private class HashNode<K, V>{
-
+    private static class HashNode<K, V> {
         private K key;
-
         private V value;
-
         private HashNode<K, V> next;
 
-        // Constructor
-
-        public HashNode(K key, V value){
-
+        public HashNode(K key, V value) {
             this.key = key;
-
             this.value = value;
-
         }
 
-
-        public String toString(){
-
+        @Override
+        public String toString() {
             return "{" + key + " " + value + "}";
-
         }
-
     }
 
-
-
-
-    private HashNode<K, V>[] chainArray; //or Object []
-
+    private HashNode<K, V>[] chainArray;
     private int M = 11;
-
     private int size;
 
-    private final double LOAD_FACTOR_THRESHOLD = 120;
-
-
     public MyHashTable() {
-
-        chainArray = new HashNode[M];
-
-        size = 0;
-
+        chainArray = (HashNode<K, V>[]) new HashNode[M];
     }
-
-
 
     public MyHashTable(int M) {
-
         this.M = M;
-
-        chainArray = new HashNode[M];
-
-        size = 0;
-
+        chainArray = (HashNode<K, V>[]) new HashNode[M];
     }
 
-
+    // Hash function to determine the index of the key in the array
     private int hash(K key) {
-
-        if (key instanceof MyTestingClass) {
-
-            MyTestingClass myKey = (MyTestingClass) key;
-
-            return Math.abs(myKey.hashCode() % M);
-
-        }
-
-        return Math.abs(key.hashCode() % M);
-
+        return (key.hashCode() & 0x7fffffff) % M;
     }
 
-   value);
+    // Insert or update a key-value pair in the hashtable
+    public void put(K key, V value) {
+        int bucketIndex = hash(key);
+        HashNode<K, V> node = chainArray[bucketIndex];
 
-        newNode.next = chainArray[index];
+        while (node != null) {
+            if (node.key.equals(key)) {  // If key already exists, update the value
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
 
-        chainArray[index] = newNode;
-
+        // Create a new node and insert it at the beginning of the bucket's linked list
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        newNode.next = chainArray[bucketIndex];
+        chainArray[bucketIndex] = newNode;
         size++;
-
-
-        double loadFactor = (double) size / M;
-
-        if (loadFactor > LOAD_FACTOR_THRESHOLD) {
-
-            resize(2 * M);
-
-        }
-
     }
 
+    // Retrieve the value associated with a given key
+    public V get(K key) {
+        int bucketIndex = hash(key);
+        HashNode<K, V> node = chainArray[bucketIndex];
 
+        while (node != null) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
+        return null; // Key not found
+    }
 
+    // Remove the key-value pair associated with a given key
+    public V remove(K key) {
+        int bucketIndex = hash(key);
+        HashNode<K, V> node = chainArray[bucketIndex];
+        HashNode<K, V> prev = null;
+
+        while (node != null) {
+            if (node.key.equals(key)) {
+                if (prev == null) {  // If the node to remove is the first node in the bucket's linked list
+                    chainArray[bucketIndex] = node.next;
+                } else {
+                    prev.next = node.next;
+                }
+                size--;
+                return node.value;
+            }
+            prev = node;
+            node = node.next;
+        }
+        return null; // Key not found
+    }
+
+    // Check if the hashtable contains a specific value
+    public boolean contains(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
+    }
+
+    // Retrieve the key associated with a specific value
+    public K getKey(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return node.key;
+                }
+                node = node.next;
+            }
+        }
+        return null; // Value not found
+    }
+
+    public static void main(String[] args) {
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>();
+        for (int i = 0; i < 10000; i++) {
+            MyTestingClass key = new MyTestingClass();
+            Student value = new Student();
+            table.put(key, value);
+        }
+
+        // Calculate the sizes of each bucket in the hashtable
+        List<Integer> bucketSizes = new ArrayList<>();
+        for (int i = 0; i < table.chainArray.length; i++) {
+            HashNode<MyTestingClass, Student> node = table.chainArray[i];
+            int size = 0;
+            while (node != null) {
+                size++;
+                node = node.next;
+            }
+            bucketSizes.add(size);
+        }
+        System.out.println("Bucket Sizes: " + bucketSizes);
+    }
+}
---
+++ MyTestingClass.java
@@ -0,0 +1,16 @@
+class MyTestingClass {    private int id;
+    private String name;    // constructor, getters, setters
+    public MyTestingClass() {        this.id = (int)(Math.random()*1000);
+        this.name = "Test-" + id;    }
+    // custom hashCode method    @Override
+    public int hashCode() {        return id % 11;
+    }}
+class Student {
+    private String name;    private int age;
+    // constructor, getters, setters
+    public Student() {
+    this.name = "zhan";
+
+    this.age = (int)(Math.random()*20 + 10);
+}
+}
